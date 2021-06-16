9bf72f575aa7354ac376a6cb25cfc061309b3536
Layner Salgado <Lainerlakers@gmail.com>
2021-06-16 09:51:27 -0500

    primer commit

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+
+import { ListadoComponent } from './listado/listado.component';
+import { HeroeComponent } from './heroe/heroe.component';
+
+
+
+@NgModule({
+    declarations: [
+      HeroeComponent,
+      ListadoComponent
+    ],
+    imports: [
+        CommonModule
+    ],
+    exports: [
+        ListadoComponent
+    ]
+   
+  })
+  export class HeroesModule { }
